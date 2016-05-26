7d31c9fb6129959372fc99c32078dacc7dc9ae1d
surgebiswas <surgebiswas@gmail.com>
2016-05-26 10:05:50 -0400

    sync commit

--- src/lag_dataset.m
+++ src/lag_dataset.m
@@ -1,12 +1,20 @@
-function z = lag_dataset( t, o )
+function [z, priors] = lag_dataset( t, o, varargin )
 % t = [samples x objects] table of nonnegative data.
 % o = [samples x 1] vector of offsets.
 
+priors = setParam(varargin, 'priors', cell(1, size(t,2)));
+
 z = zeros(size(t));
+
 for i = 1 : size(t,2)
-    prior.mu = mean(log(t(:,i)./o + 1));
-    prior.sig2 = var(log(t(:,i)./o + 1));
-    [z(:,i), pl] = lag_plus_learn_prior(t(:,i),o,prior);
+    
+    if isempty(priors{i})
+        prior.mu = mean(log(t(:,i)./o + 1));
+        prior.sig2 = var(log(t(:,i)./o + 1));
+        [z(:,i), priors{i}] = lag_plus_learn_prior(t(:,i),o,prior);
+    else
+        z(:,i) = lag(t(:,i), o, priors{i});
+    end
     
     if mod(i,100) == 0
         disp(i);
@@ -15,4 +23,5 @@ end
 
 
 end
+
 
